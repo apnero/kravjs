96160fb77395203c9e3d811cd8dfe4a1e20ef146
Andrew Nero <andrew.nero@gmail.com>
2014-02-10 23:17:53 -0500

    new look

---
+++ model/kravsliderjoin.js
@@ -0,0 +1,224 @@
+{
+
+"metadata" :
+{
+	"formatVersion" : 3.2,
+	"type"          : "scene",
+	"sourceFile"    : "kravsliderjoin.blend",
+	"generatedBy"   : "Blender 2.65 Exporter",
+	"objects"       : 9,
+	"geometries"    : 6,
+	"materials"     : 4,
+	"textures"      : 3
+},
+
+"urlBaseType" : "relativeToScene",
+
+
+"objects" :
+{
+	"Text.005" : {
+		"geometry"  : "geo_SDMesh",
+		"groups"    : [  ],
+		"material"  : "Material.003",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Text.004" : {
+		"geometry"  : "geo_IntelligenceMesh",
+		"groups"    : [  ],
+		"material"  : "Material.003",
+		"position"  : [ 0, 1.67884, 2.83032e-07 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Text.003" : {
+		"geometry"  : "geo_FitnessMesh",
+		"groups"    : [  ],
+		"material"  : "Material.003",
+		"position"  : [ 0.076311, 3.39584, 5.72496e-07 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Krav" : {
+		"groups"    : [  ],
+		"position"  : [ 0.38642, -2.39966, -0.301801 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 5.26933, 5.26933, 5.26933 ]
+	},
+
+	"Letters" : {
+		"geometry"  : "geo_LettersMesh",
+		"groups"    : [  ],
+		"material"  : "Material.002",
+		"position"  : [ 0.360038, -2.34722, -0.301801 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 27.7535, 27.7535, 27.7535 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Shield" : {
+		"geometry"  : "geo_ShieldMesh",
+		"groups"    : [  ],
+		"material"  : "Material.001",
+		"position"  : [ 0.374537, -2.27172, -0.301801 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 27.7535, 27.7535, 27.7535 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Leaves" : {
+		"geometry"  : "geo_LeavesMesh",
+		"groups"    : [  ],
+		"material"  : "Material",
+		"position"  : [ -1.84564, -1.38854, -0.301801 ],
+		"rotation"  : [ -4.12694e-08, 0, 0 ],
+		"quaternion": [ -2.06347e-08, 0, 0, 1 ],
+		"scale"     : [ 27.7535, 27.7535, 27.7535 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"Lamp" : {
+		"type"       : "PointLight",
+		"position"   : [ 19.0038, 28.696, 6.46961 ],
+		"rotation"   : [ 0.650328, 1.86639, 0.0552171 ],
+		"color"      : 16777215,
+		"distance"   : 30.000,
+		"intensity"  : 1.000
+	},
+
+	"Camera" : {
+		"type"  : "PerspectiveCamera",
+		"fov"   : 49.159264,
+		"aspect": 1.333000,
+		"near"  : 0.100000,
+		"far"   : 100.000000,
+		"position": [ 36.9373, 25.7454, 33.1018 ],
+		"target"  : [ 0, 0, 0 ]
+	}
+},
+
+
+"geometries" :
+{
+	"geo_SDMesh" : {
+		"type" : "ascii",
+		"url"  : "kravsliderjoin.SDMesh.js"
+	},
+
+	"geo_IntelligenceMesh" : {
+		"type" : "ascii",
+		"url"  : "kravsliderjoin.IntelligenceMesh.js"
+	},
+
+	"geo_FitnessMesh" : {
+		"type" : "ascii",
+		"url"  : "kravsliderjoin.FitnessMesh.js"
+	},
+
+	"geo_LettersMesh" : {
+		"type" : "ascii",
+		"url"  : "kravsliderjoin.LettersMesh.js"
+	},
+
+	"geo_ShieldMesh" : {
+		"type" : "ascii",
+		"url"  : "kravsliderjoin.ShieldMesh.js"
+	},
+
+	"geo_LeavesMesh" : {
+		"type" : "ascii",
+		"url"  : "kravsliderjoin.LeavesMesh.js"
+	}
+},
+
+
+"textures" :
+{
+	"RockSharp0137_M_d.jpg" : {
+		"url": "RockSharp0137_M_d.jpg",
+        "wrap": ["repeat", "repeat"]
+	},
+
+	"RockSharp0137_M_n.jpg" : {
+		"url": "RockSharp0137_M_n.jpg",
+        "wrap": ["repeat", "repeat"]
+	},
+
+	"RockSharp0137_M_s.jpg" : {
+		"url": "RockSharp0137_M_s.jpg",
+        "wrap": ["repeat", "repeat"]
+	}
+},
+
+
+"materials" :
+{
+	"Material" : {
+		"type": "MeshPhongMaterial",
+		"parameters": { "color": 13421772, "ambient": 13421772, "opacity": 1, "ambient": 13421772, "specular": 8355711, "shininess": 5e+01, "map": "RockSharp0137_M_d.jpg", "specularMap": "RockSharp0137_M_s.jpg", "bumpMap": "RockSharp0137_M_n.jpg", "bumpMapScale": 0.5, "blending": "NormalBlending" }
+	},
+
+	"Material.001" : {
+		"type": "MeshPhongMaterial",
+		"parameters": { "color": 210688, "ambient": 210688, "opacity": 1, "ambient": 210688, "specular": 8355711, "shininess": 5e+01, "map": "RockSharp0137_M_d.jpg", "specularMap": "RockSharp0137_M_s.jpg", "bumpMap": "RockSharp0137_M_n.jpg", "bumpMapScale": 0.5, "vertexColors": "vertex", "blending": "NormalBlending" }
+	},
+
+	"Material.002" : {
+		"type": "MeshPhongMaterial",
+		"parameters": { "color": 13421772, "ambient": 13421772, "opacity": 1, "ambient": 13421772, "specular": 8355711, "shininess": 5e+01, "map": "RockSharp0137_M_d.jpg", "specularMap": "RockSharp0137_M_s.jpg", "bumpMap": "RockSharp0137_M_n.jpg", "bumpMapScale": 0.5, "blending": "NormalBlending" }
+	},
+
+	"Material.003" : {
+		"type": "MeshPhongMaterial",
+		"parameters": { "color": 32512, "ambient": 32512, "opacity": 1, "ambient": 32512, "specular": 8355711, "shininess": 5e+01, "map": "RockSharp0137_M_d.jpg", "specularMap": "RockSharp0137_M_s.jpg", "bumpMap": "RockSharp0137_M_n.jpg", "bumpMapScale": 0.5, "blending": "NormalBlending" }
+	}
+},
+
+
+"transform" :
+{
+	"position"  : [ 0, 0, 0 ],
+	"rotation"  : [ 0, 0, 0 ],
+	"scale"     : [ 1, 1, 1 ]
+},
+
+"defaults" :
+{
+	"bgcolor" : [ 0, 0, 0 ],
+	"bgalpha" : 1.000000,
+	"camera"  : "Camera"
+}
+
+}
